a5f161d74925b6282fd065bb0eeb581531a2ec9b
Artem <artemradckevich@gmail.com>
2026-05-10 10:49:25 +0300

    add custom user (Worker) model with position relation

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        related_name="workers",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} ({self.position})"

6f83b36790fca9257bdc0898acd77958aacf294f
Artem <artemradckevich@gmail.com>
2026-05-10 11:16:27 +0300

    add Task and TaskType models with relations

--- tasks/models.py
+++ tasks/models.py
@@ -16,8 +16,41 @@ class Worker(AbstractUser):
     position = models.ForeignKey(
         Position,
         related_name="workers",
-        on_delete=models.CASCADE
+        on_delete=models.PROTECT
     )
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} ({self.position})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=20,
+        choices=[
+            ("URGENT", "Urgent"),
+            ("HIGH", "High"),
+            ("MEDIUM", "Medium"),
+            ("LOW", "Low"),
+            ("OPTIONAL", "Optional")
+        ],
+        default="MEDIUM"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        related_name="tasks",
+        on_delete=models.PROTECT
+    )

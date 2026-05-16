b24896a0fb8efb2ffa5448ce7202cb4e293d6fe9
Artem <artemradckevich@gmail.com>
2026-05-16 14:13:23 +0300

    Fix static files issues

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -88,7 +88,6 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ASSETS_ROOT = "/static/assets"
 
--- task_manager/settings/prod.py
+++ task_manager/settings/prod.py
@@ -10,6 +10,8 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
  'default': {
